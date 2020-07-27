4b5b8057d2853f3ce7f7824694a1f4d61e715918
quantuumsnot <quantuumsnot@users.noreply.github.com>
2020-07-27 18:58:09 +0300

    Some improvements to the AJAX and PHP logic

--- index.php
+++ index.php
@@ -9,13 +9,40 @@ gc_enable(); //enables Garbage Collector
 /*IMPORTANT - after each include of external file get the memory allocated by OS
  with memory_get_peak_usage(true) to get correct memory usage*/
 
-//try { }
-//catch(Exception $e) { echo 'Message: ' . $e->getMessage(); }
+header("Content-type: text/plain");
+
+// Simple IP filtering, not hacker-resistant
+$whitelist = array("127.0.0.1", "192.168.100.3"); // list of allowed IPs
+if(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
+  echo $_SERVER['REMOTE_ADDR'];
+  exit("This IP address is banned from accessing this service");
+}
 
 //global vars
 $phpVersionNumber = PHP_VERSION_ID;
 $phpVersionType = PHP_INT_SIZE * 8 . "bit";
 //$div = "<div class='center'>{{content}}</div>";
+$name = $_POST['name'];
+$number = $_POST['number'];
+$category = $_POST['category'];
+$quantity = $_POST['quantity'];
+$available = $_POST['available'];
+$price = $_POST['price'];
+$info = $_POST['info'];
+
+// Open database
+$productsColumns = "name TEXT, number TEXT, category TEXT, quantity TEXT, available TEXT, price TEXT, info TEXT";//pictures BLOB";
+$productsColumnsNoDataType = "name, number, category, quantity, available, price, info";
+$salesColumns = "date TEXT, number TEXT, quantity TEXT";
+$salesColumnsNoDataType = "date, number, quantity";
+$db = new PDO('sqlite:sics.db');
+//PHP bug: if you don't specify PDO::PARAM_INT for INT values, PDO may enclose the argument in quotes. This can mess up some MySQL queries that don't expect integers to be quoted.
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_PERSISTENT, false);
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //disable emulated prepares to prevent injection, see http://stackoverflow.com/a/12202218/1196983
+$db->exec("CREATE TABLE IF NOT EXISTS {$_POST['tName']}({$productsColumns})");
+$db->exec("CREATE TABLE IF NOT EXISTS `sales`(${salesColumns})");
+
 
 //Peak memory usage for output, see http://stackoverflow.com/a/2510468/1196983
 function getMemoryUsageUnits($bytes) {
@@ -26,23 +53,34 @@ function getMemoryUsageUnits($bytes) {
   } else return 0;
 }
 
-//save test results to database
+// Search for item in the database
+//$statement = $pdo->query("SELECT some_field FROM some_table");
+
+// Add item to database
 try {
-  $columns = "name TEXT, number TEXT, category TEXT, quantity TEXT, available TEXT, pictures BLOB";
-  $pdo = new PDO('sqlite:sics.db');
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $pdo->setAttribute(PDO::ATTR_PERSISTENT, false);
-  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //disable emulated prepares to prevent injection, see http://stackoverflow.com/a/12202218/1196983
-  //$statement = $pdo->query("SELECT some_field FROM some_table");
-  $pdo->exec("CREATE TABLE IF NOT EXISTS {$_POST['tName'])}({$columns})");
-  $pdo = $pdo->prepare("INSERT INTO {$_POST['tName']} (name, number, category, quantity, available, pictures) values (?, ?, ?, ?, ?, ?)");
-  //PHP bug: if you don't specify PDO::PARAM_INT for INT values, PDO may enclose the argument in quotes. This can mess up some MySQL queries that don't expect integers to be quoted.
-  $pdo->execute();
-  $pdo = null;
-  unset($pdo); //close database handler
-  //$row = $pdo->fetch(PDO::FETCH_ASSOC);
-  //echo "Done!<br />"; //just for testing, leave it commented to stop pollution with echoes
-} catch(PDOException $e) { echo $e->getMessage(); $pdo = null; unset($pdo); /*and close database handler*/ }
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $result = $db->prepare("INSERT INTO {$_POST['tName']} ({$productsColumnsNoDataType}) VALUES (?, ?, ?, ?, ?, ?, ?)");
+    $result->bindValue(1, $name);
+    $result->bindValue(2, $number);
+    $result->bindValue(3, $category);
+    $result->bindValue(4, $quantity);
+    $result->bindValue(5, $available);
+    $result->bindValue(6, $price);
+    $result->bindValue(7, $info);
+    /*$result->bindValue(7, $_POST['pictures']);*/
+    $result->execute();
+    //$result = null;
+    //unset($db, $result); //close database handler
+    //$row = $db->fetch(PDO::FETCH_ASSOC);
+    //echo "Done!<br />"; //just for testing, leave it commented to stop pollution with echoes
+    //print_r($db->errorInfo());
+    //header("Location: ../index.html");
+    echo "Done! <br />";
+  }
+} catch(PDOException $e) { echo $e->getMessage(); $db = null; unset($db, $result); /*and close database handler*/ }
+
+// Sell item and exclude it from the database
+//blabla
 
 //$pageStart = microtime(true);
 
@@ -50,7 +88,7 @@ try {
 echo $tempDiv . "<br />";*/
 
 //show which web-server you're using
-echo $webserver = isset($_SERVER['SERVER_SOFTWARE']) ? $_SERVER['SERVER_SOFTWARE'] : phpversion();
+//echo $webserver = isset($_SERVER['SERVER_SOFTWARE']) ? $_SERVER['SERVER_SOFTWARE'] : phpversion();
 
 /*echo "<p style='text-align: center'>Webserver: PHP {$webserver} ... PHP Version: " . phpversion() . "<br />
 Report table generated for " . number_format(microtime(true) - $pageStart, 3) . " seconds on " . date('d M Y @ H:i:s');*/
--- sics.js
+++ sics.js
@@ -1,74 +1,67 @@
 'use strict';
 
-// Create needed constants
-const pName = document.getElementsByName('productName');
-const pNumber = document.getElementsByName('productNumber');
-const pCategory = document.getElementsByName('productCategory');
-const pQuantity = document.getElementsByName('productQuantity');
-const pAvailable = document.getElementsByName('productAvailable');
-const pPictures = document.getElementsByName('productPictures');
-const pAdd = document.getElementsByName('addproduct');
-
-
-// Add products to the database
-function addProduct(e) {
-  // Prevent default - we don't want the form to submit in the conventional way
-  e.preventDefault();
-
-  // Grab the values entered into the form fields and store them in an object ready for being inserted into the DB
-  let newProduct = { 	productName: 			pName.value, 
-											productNumber: 		pNumber.value, 
-											productCategory: 	pCategory.value, 
-											productQuantity: 	pQuantity.value,
-                      productAvailable: pAvailable, 
-                      productPictures: 	pPictures.value
-										};
+function addProduct() {
+  var name      =  document.getElementById('name').value;
+  var number    =  document.getElementById('number').value;
+  var category  =  document.getElementById('category').value;
+  var quantity  =  document.getElementById('quantity').value;
+  var available =  document.getElementById('available').value;
+  var price =  document.getElementById('price').value;
+  var info      =  document.getElementById('info').value;
 
   // Make a request to add the product
-  var request = function () {
-    xhttp = new XMLHttpRequest();
-    
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("searchresults").innerHTML = this.responseText;
+  var xhttp = new XMLHttpRequest();
+  
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4) {
+      if (this.status == 200) {
+        document.getElementById("newproductresults").innerHTML = this.responseText;
       }
-    };
+      else {
+        alert("Error: returned status code " + this.status + " " + this.statusText);
+      }
+    }
+  };
     
-    xhttp.open("POST", "http://127.0.0.1:8000/sics/index.php", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send("tName=" + "products" + 
-               "&pName=" + newProduct.productName + 
-               "&pNumber=" + newProduct.productNumber + 
-               "&pCategory=" + newProduct.productCategory + 
-               "&pQuantity=" + newProduct.productQuantity + 
-               "&pAvailable=" + newProduct.productAvailable + 
-               "&pPictures=" + newProduct.productPictures);
-  };
+  xhttp.open("POST", "index.php", true);
   
-  request();
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   
-  // On successfull insert, clear the form to be ready for adding a new entry
-  request.onsuccess = function() {
-		pName.value = '';
-		pNumber.value = '';
-		pCategory.value = '';
-		pQuantity.value = '';
-    pAvailable.value = '';
-    pPictures.value = '';
-  };
-
-  // Report when there's a problem with adding new product
-  request.onerror = function() {
-    console.log('Transaction not opened due to an error');
-  };
+  if (name !== '' && 
+      number !== '' && 
+      category !== '' && 
+      quantity !== '' && 
+      available !== '' && 
+      price !== '' && 
+      info !== '') {
+        xhttp.send("tName=products" + 
+                   "&name="       + name + 
+                   "&number="     + number + 
+                   "&category="   + category + 
+                   "&quantity="   + quantity + 
+                   "&available="  + available + 
+                   "&price="      + price + 
+                   "&info="       + info); /*+ 
+                   "&pictures=" + data.pictures);*/
+                   
+        document.getElementById('name').value = '';
+        document.getElementById('number').value = '';
+        document.getElementById('category').value = '';
+        document.getElementById('quantity').value = '';
+        document.getElementById('available').value = '';
+        document.getElementById('price').value = '';
+        document.getElementById('info').value = '';
+  } else {
+      alert("Error: Some fields are empty!");
+  }
 }
 
 // Search for a product in the database
 function searchProduct() {
   
-  executerequest();
+  //executerequest();
   
-  executerequest.onsuccess = function(e) {
+  /*executerequest.onsuccess = function(e) {
     if(e.state === true) {
       // Create a list item, h3, and p to put each data item inside when displaying it
       // structure the HTML fragment, and append it inside the list
@@ -102,10 +95,47 @@ function searchProduct() {
         list.appendChild(listItem);
       }
     }
-  };
+  };*/
 }
 
-window.onload = function() {
-	pAdd.onclick = addProduct();
-  //pSearch.onclick = displayProducts;
-};
+// Sell a product and update the quantity left in the database
+function sellProduct() {
+  
+  //executerequest();
+  
+  /*executerequest.onsuccess = function(e) {
+    if(e.state === true) {
+      // Create a list item, h3, and p to put each data item inside when displaying it
+      // structure the HTML fragment, and append it inside the list
+      let listItem = document.createElement('li');
+      let h3 = document.createElement('h3');
+      let para = document.createElement('p');
+
+      listItem.appendChild(h3);
+      listItem.appendChild(para);
+      list.appendChild(listItem);
+
+      // Put the data from the cursor inside the h3 and para
+      h3.textContent = cursor.value.title;
+      para.textContent = cursor.value.body;
+
+      // Create a button and place it inside each listItem
+      let deleteBtn = document.createElement('button');
+      listItem.appendChild(deleteBtn);
+      deleteBtn.textContent = 'Delete';
+
+      // Add an event handler for deleting a product from the database
+      deleteBtn.onclick = deleteProduct;
+
+      // Iterate to the next item in the cursor
+      cursor.continue();
+    } else {
+      // Again, if list item is empty, display a 'No notes stored' message
+      if(!list.firstChild) {
+        let listItem = document.createElement('li');
+        listItem.textContent = 'No products found';
+        list.appendChild(listItem);
+      }
+    }
+  };*/
+}
