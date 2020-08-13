8a0fef05c1eaa271a16cd15723987e2f9e45addb
quantuumsnot <quantuumsnot@users.noreply.github.com>
2020-08-14 00:04:26 +0300

    Added initial changes for interface localizations
    Next mini TODO will be adding a button for language change

--- sics.js
+++ sics.js
@@ -1,5 +1,83 @@
 'use strict';
 
+console.clear();
+
+var langs = ["EN", "BG", "RU", "FR", "DE"];
+var localization = "BG";
+
+function setLocalization() {
+  if (langs.includes(localization)) {
+    var x = document.querySelectorAll("input");
+    var y = document.querySelectorAll("label");
+    
+    var i, j;
+    
+    // Localize input fields
+    for (i of x) {
+      switch (i.placeholder) {
+        case "Name":                    i.placeholder = "Име на продукта"; break;
+        case "Category":                i.placeholder = "Категория"; break;
+        case "Additional product info": i.placeholder = "Информация за продукта"; break;
+        case "SKU":                     i.placeholder = "Артикулен номер"; break;
+        case "Quantity":                i.placeholder = "Брой"; break;
+        case "Available":               i.placeholder = "Наличност в склада"; break;
+        case "Price":                   i.placeholder = "Цена"; break;
+        case "Customer names":          i.placeholder = "Имена на клиента"; break;
+        case "Customer Phone Number":   i.placeholder = "Телефонен номер на клиента"; break;
+        case "Customer Address":        i.placeholder = "Адрес на клиента"; break;
+      }
+    }
+    
+    // Localize labels
+    for (j of y) {
+      switch (j.getAttribute("for")) {
+        case "tabone":   j.innerHTML = "Добави нов продукт"; break;
+        case "tabtwo":   j.innerHTML = "Търсене на продукт"; break;
+        case "tabthree": j.innerHTML = "Продай"; break;
+        case "tabfour":  j.innerHTML = "Некоректни клиенти"; break;
+        case "tabfive":  j.innerHTML = "Добави некоректен клиент"; break;
+      }
+    }
+    
+    // Localize action buttons
+    document.getElementById("addproduct").innerHTML       = "ЗАПИС";
+    document.getElementById("searchproduct").innerHTML    = "ТЪРСИ";
+    document.getElementById("sellproduct").innerHTML      = "ОТЧЕТИ ПРОДАЖБА";
+    document.getElementById("customerbanlist").innerHTML  = "ТЪРСИ";
+    document.getElementById("bancustomer").innerHTML      = "ЗАПИС";
+    
+    // Localize drop-down select button for Sell action
+    var soldinselect = document.getElementById("soldin").options;
+    for (let aaa of soldinselect) {
+      switch (aaa.value) {
+        case "a":     aaa.textContent = "Избери къде е продаден продукта"; break;
+        case "store": aaa.textContent = "МАГАЗИН"; break;
+        case "site":  aaa.textContent = "САЙТ"; break;
+        case "fb":    aaa.textContent = "Фейсбук"; break;
+        case "olx":   aaa.textContent = "ОЛХ"; break;
+        case "bazar": aaa.textContent = "Базар.БГ"; break;
+        case "phone": aaa.textContent = "ТЕЛЕФОН"; break;
+        case "viber": aaa.textContent = "Вайбър"; break;
+      }
+    }
+    
+    // Localize drop-down select button for Ban Customer action
+    var wherewasorderedselect = document.getElementById("wherewasordered").options;
+    for (let bbb of wherewasorderedselect) {
+      switch (bbb.value) {
+        case "a":     bbb.textContent = "Избери от къде е поръчан продукта"; break;
+        case "store": bbb.textContent = "МАГАЗИН"; break;
+        case "site":  bbb.textContent = "САЙТ"; break;
+        case "fb":    bbb.textContent = "Фейсбук"; break;
+        case "olx":   bbb.textContent = "ОЛХ"; break;
+        case "bazar": bbb.textContent = "Базар.БГ"; break;
+        case "phone": bbb.textContent = "ТЕЛЕФОН"; break;
+        case "viber": bbb.textContent = "Вайбър"; break;
+      }
+    }
+  }
+}
+
 function addProduct() {
   var name      =  document.getElementById('name').value;
   var number    =  document.getElementById('number').value;
@@ -316,3 +394,4 @@ function checkMessages() {
   xhttp.send(formData);
 }
 
+setLocalization();
